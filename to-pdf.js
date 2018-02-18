3a9fee0d3db1bbb69e6748cd620252d7667bbb11
Devin Clark <dclarkdesign@gmail.com>
2018-02-17 18:33:09 -0600

    circleci testing.

---
+++ to-pdf.js
@@ -0,0 +1,29 @@
+const puppeteer = require('puppeteer');
+const serve = require('serve');
+
+const server = serve(__dirname, {
+  port: 1337,
+  clipless: true,
+  silent: true,
+  ignore: ['node_modules']
+});
+
+// this isn't important enough to figure out the race condition.
+setTimeout(async () => {
+  const browser = await puppeteer.launch();
+  const page = await browser.newPage();
+  await page.goto('http://localhost:1337', {waitUntil: 'networkidle2'});
+  await page.pdf({
+    path: 'devin-clark-resume.pdf',
+    format: 'Letter',
+    margin: {
+      top: '0.25in',
+      bottom: '0.25in',
+      left: '0.25in',
+      right: '0.25in',
+    }
+  });
+
+  await browser.close();
+  server.stop();
+}, 5000);
