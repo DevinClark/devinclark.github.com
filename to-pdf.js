d3386a895ee823e748af8e8530a4654b23a182b4
Devin Clark <dclarkdesign@gmail.com>
2018-02-17 19:09:41 -0600

    Error handling. maybe do artifacts correctly.

--- to-pdf.js
+++ to-pdf.js
@@ -10,20 +10,26 @@ const server = serve(__dirname, {
 
 // this isn't important enough to figure out the race condition.
 setTimeout(async () => {
-  const browser = await puppeteer.launch();
-  const page = await browser.newPage();
-  await page.goto('http://localhost:1337', {waitUntil: 'networkidle2'});
-  await page.pdf({
-    path: 'devin-clark-resume.pdf',
-    format: 'Letter',
-    margin: {
-      top: '0.25in',
-      bottom: '0.25in',
-      left: '0.25in',
-      right: '0.25in',
-    }
-  });
+  try {
+    const browser = await puppeteer.launch();
+    const page = await browser.newPage();
+    await page.goto('http://localhost:1337', {waitUntil: 'networkidle2'});
+    await page.pdf({
+      path: 'devin-clark-resume.pdf',
+      format: 'Letter',
+      margin: {
+        top: '0.25in',
+        bottom: '0.25in',
+        left: '0.25in',
+        right: '0.25in',
+      }
+    });
 
-  await browser.close();
-  server.stop();
+    await browser.close();
+    server.stop();
+  } catch(ex) {
+    server.stop();
+    console.log(ex)
+    process.exit(1);
+  }
 }, 5000);
